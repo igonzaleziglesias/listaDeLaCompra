aba50d49488cba67c978714eef1e2344ba89bc17
Iago <igonzaleziglesias@danielcastelao.org>
2020-02-13 11:53:01 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CorreoComponent } from './Components/correo/correo.component';
+import { ListaCorreosComponent } from './Components/lista-correos/lista-correos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CorreoComponent,
+    ListaCorreosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
